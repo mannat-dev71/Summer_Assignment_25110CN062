9960632ecc50c37b9357d96f91a04f5588636716
mannat-dev71 <mannat7505490526@gmail.com>
2026-06-07 12:06:50 +0530

    DAY-7 uploaded

---
+++ DAY-7/recursive_sumofdigits.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int sumofdigit(int n){
+    if(n == 0){
+        return 0;
+    }
+    return n%10 + sumofdigit(n/10);
+}
+
+int main(){
+    int n;
+    cout<<"Enter the number= ";
+    cin>>n;
+    cout<<"Sum of digits will be= "<<sumofdigit(n)<<endl;
+    return 0;
+
+}
