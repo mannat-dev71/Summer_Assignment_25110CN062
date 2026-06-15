0696379becb3d92cd950ece5ffe5e2707f66e3b3
mannat-dev71 <mannat7505490526@gmail.com>
2026-06-15 22:33:41 +0530

    DAY-15 uploaded

---
+++ DAY-15/left_rotation.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size of array = ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter array elements: ";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int temp = arr[0];
+
+    for(int i = 0; i < n - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+
+    arr[n - 1] = temp;
+
+    cout << "Array after left rotation = ";
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
