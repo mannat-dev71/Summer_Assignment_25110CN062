068cc7198fca50e905f8be9686655485d08f7549
mannat-dev71 <mannat7505490526@gmail.com>
2026-06-19 22:29:52 +0530

    DAY-19 uploaded

---
+++ DAY-19/subsMatrices.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a[3][3], b[3][3];
+
+    cout<<"Enter first matrix:\n";
+    for(int i=0;i<3;i++)
+        for(int j=0;j<3;j++)
+            cin>>a[i][j];
+
+    cout<<"Enter second matrix:\n";
+    for(int i=0;i<3;i++)
+        for(int j=0;j<3;j++)
+            cin>>b[i][j];
+
+    cout<<"Subtraction Matrix:\n";
+    for(int i=0;i<3;i++) {
+        for(int j=0;j<3;j++) {
+            cout<<a[i][j]-b[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
