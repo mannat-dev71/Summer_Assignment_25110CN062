2b88c836ef99214396850e9be9fe405a702bd615
mannat-dev71 <mannat7505490526@gmail.com>
2026-06-25 22:48:24 +0530

    DAY-25 uploaded

---
+++ DAY-25/ques_97.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,m;
+
+    cin>>n;
+    int a[n];
+
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+
+    cin>>m;
+    int b[m];
+
+    for(int i=0;i<m;i++){
+        cin>>b[i];
+    }
+
+    int i=0,j=0;
+
+    while(i<n && j<m){
+        if(a[i]<b[j]){
+            cout<<a[i]<<" ";
+            i++;
+        }
+        else{
+            cout<<b[j]<<" ";
+            j++;
+        }
+    }
+
+    while(i<n){
+        cout<<a[i]<<" ";
+        i++;
+    }
+
+    while(j<m){
+        cout<<b[j]<<" ";
+        j++;
+    }
+
+    return 0;
+}
---
+++ DAY-25/ques_98.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string s1,s2;
+
+    cin>>s1>>s2;
+
+    for(int i=0;i<s1.length();i++){
+        for(int j=0;j<s2.length();j++){
+            if(s1[i]==s2[j]){
+                cout<<s1[i];
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
